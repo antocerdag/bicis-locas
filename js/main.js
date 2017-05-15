b04505b3278c858ad711f7193f1d0d777ab78ce7
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-15 11:40:21 -0400

    llamando ids

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var nombre = document.getElementById("name").value;
+	var apellido = document.getElementById("lastname").value;
+	var email = document.getElementById("input-email").value;
+	var password = document.getElementById("password").value;
+
+	
 }
