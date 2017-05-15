2a5c680fa5c89846f3b0b1ed42b464c98e76028f
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-15 13:00:57 -0400

    validando password

--- js/main.js
+++ js/main.js
@@ -5,13 +5,25 @@ function validateForm(){
 	var password = document.getElementById("input-password").value;
 	var select = document.getElementsByClassName("form-control").selectedIndex;
 
-	if( select == null || select == 0 ) {
-  		alert("Debe seleccionar su bicicleta");
+
+	/*if(!(/^[A-Z][a-z]{3,19}$/).test(nombre)){
+		alert("Ingrese un nombre válido");
+	} if(!!(/^[A-Z][a-z]{3,19}$/).test(apellido)){
+		alert("Ingrese un apellido válido");
 	} if(!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email))){
 		alert("Ingrese un correo valido");
-	} if((nombre.length == 0) || (apellido.length == 0) || (email.length == 0) || (password.length == 0)){
-		alert("Debe completar todos los campos");
+	}*/ if((password == "password") || (password == 123456) || (password == 098754) || (!(/^([0-9].{6,15}$/).test(password)) ){
+		alert("Ingrese una clave válida");
 	}
 
+
+	/*if( select == null || select == 0 ) {
+  		alert("Debe seleccionar su bicicleta");
+	} 
+
+	if((nombre.length == 0) || (apellido.length == 0) || (email.length == 0) || (password.length == 0)){
+		alert("Debe completar todos los campos");
+	}
+*/
 
 }
