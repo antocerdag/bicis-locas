dff8992af8c58be8a71dbf65e6be15c9cef48370
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-15 12:06:10 -0400

    creando if para validar campos

--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@ function validateForm(){
 	var nombre = document.getElementById("name").value;
 	var apellido = document.getElementById("lastname").value;
 	var email = document.getElementById("input-email").value;
-	var password = document.getElementById("password").value;
+	var password = document.getElementById("input-password").value;
+	var select = document.getElementsByClassName("form-control").selectedIndex;
 
-	
+	if( select == null || select == 0 ) {
+  		alert("Debe seleccionar su bicicleta");
+	} if(!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email))){
+		alert("Ingrese un correo valido");
+	} if((nombre.length == 0) || (apellido.length == 0) || (email.length == 0) || (password.length == 0)){
+		alert("Debe completar todos los campos");
+	}
+
+
 }
